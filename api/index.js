565c14880c76d41f63ccda1bfeef9f2b16e411c8
luisjonath <luisjonacardenas@gmail.com>
2023-04-07 14:59:32 -0300

    backend configurado

--- api/index.js
+++ api/index.js
@@ -1,17 +1,40 @@
 const express = require("express")
+require("dotenv").config()
+const {dbConnection} = require("./database/config")
+const cors = require("cors")
 
 
+
+//Servidor de express
 const app = express()
 
+//Base de datos
+dbConnection()
 
-app.get("/", (req, res)=>{
+//Cors
+
+app.use(cors())
+
+//Directorio publico
+app.use(express.static("public"))
+
+//Parseo del body
+
+app.use(express.json())
+
+
+// Rutas
+app.use("/api/auth", require("./routes/auth"))
+
+/* app.get("/", (req, res)=>{
     console.log("Se requiere el /")
     res.json({
         ok:true
     })
 })
+ */
 
-
-app.listen(4000, () => {
-    console.log(`Servidor corriendo en puerto ${4000}`)
+//Peticiones
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}`)
 })
